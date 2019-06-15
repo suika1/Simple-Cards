6cd0a5c9dae2dba5d007e65eae644d205822c9ac
suika1 <vanyoktank1@gmail.com>
2019-06-15 17:44:25 +0300

    add generateResponse util

---
+++ backend/src/utils/index.js
@@ -0,0 +1,18 @@
+export const generateResponse = ({
+    res,
+    status = 200,
+    results = '',
+    error = '',
+    ok = true,
+    ...another
+  }) => {
+    if (error && error.length) ok = false;
+    return res.status(status).send(
+      JSON.stringify({
+        results,
+        ok,
+        error,
+        ...another,
+      }),
+    );
+  };
