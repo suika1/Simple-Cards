4cfd7478816c51295788339a3907d372c71dce6d
suika1 <vanyoktank1@gmail.com>
2019-06-17 00:26:58 +0300

    add app reducer

--- frontend/src/reducers.js
+++ frontend/src/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router'
 
-import loginPage from './modules/login-page/reducer';
-import cardList from './modules/card-list/reducer';
+import loginPage from 'modules/login-page/reducer';
+import cardList from 'modules/card-list/reducer';
+import app from 'modules/app/reducer';
 
 export default history => combineReducers({
+  app,
   loginPage,
   cardList,
   router: connectRouter(history),
