317b3b03b0f539afbcb4da9764517a75f1546fa3
suika1 <vanyoktank1@gmail.com>
2019-06-16 17:28:00 +0300

    add validate-by-token route

--- backend/src/routes/api/user.js
+++ backend/src/routes/api/user.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import * as UserController from '../../controllers/user';
+import middlewares from '../../middlewares';
 
 const router = express.Router();
 
 router.post('/', UserController.createUser);
 
 router.post('/validate/', UserController.validateUser);
+router.post('/validate-by-token/', middlewares.checkToken, UserController.validateByToken);
 
 export default router;
