6872d5437b104ccdaf7ee125bf56498ebbc019c7
suika1 <vanyoktank1@gmail.com>
2019-06-17 00:23:40 +0300

    test email by regexp

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -21,6 +21,15 @@ export const createUser = async (req, res, next) => {
       error: 'Bad arguments',
     });
 
+    const emailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!emailRegexp.test(email)) return utils.generateResponse({
+      res,
+      status: 400,
+      ok: false,
+      error: 'Bad email',
+    });
+
     // check if exists
     const foundUser = await User.findOne({
       where: {
