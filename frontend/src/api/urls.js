c5e0124c1b3ec96c76cb7df83ec0cbca76b2f2f9
suika1 <vanyoktank1@gmail.com>
2019-06-17 00:27:40 +0300

    add urls

---
+++ frontend/src/api/urls.js
@@ -0,0 +1,8 @@
+const host = 'http://127.0.0.1:5000/';
+const api = `${host}api/v1/`;
+
+export const cards = `${api}cards/`;
+export const users = `${api}users/`;
+
+export const loginUser = `${users}validate/`;
+export const validateUserByToken = `${users}validate-by-token/`;
